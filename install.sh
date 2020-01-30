f337b2a15cf8072d936a4b1558cc473e6a7f7b3d
Thiago Tognoli <thiagotognoli@users.noreply.github.com>
2020-01-30 11:32:29 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -193,6 +193,7 @@ function install_lsd() {
     && sudo -u $currentUser wget https://github.com/Peltoche/lsd/releases/download/0.16.0/lsd_0.16.0_amd64.deb -O "$currentHomeDir/tmp/lsd.deb" \
     && sudo dpkg -i "$currentHomeDir/tmp/lsd.deb" \
     && sudo -u $currentUser rm -rf "$currentHomeDir/tmp"
+    #sudo snap install lsd
 }
 
 function config_gnomeshell() {
