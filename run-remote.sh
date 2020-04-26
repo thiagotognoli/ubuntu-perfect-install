847b39b73008e1b7c0aa317db8b0ccb6b45ce9ce
Thiago Tognoli <thiagotognoli@users.noreply.github.com>
2020-04-26 15:07:10 -0300

    Update run-remote.sh

--- run-remote.sh
+++ run-remote.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 pkexec env DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY bash -c "\
-cd /tmp \
+  sudo rm -rf /tmp/ubuntu-perfect-install \
   && sudo apt install -y git \
-  && git clone https://github.com/thiagotognoli/ubuntu-perfect-install.git \
-  && cd ubuntu-perfect-install \
-  && sudo ./run.sh \
-  && cd ~ \
-  && rm -rf /tmp/ubuntu-perfect-install\
+  && git clone https://github.com/thiagotognoli/ubuntu-perfect-install.git /tmp/ubuntu-perfect-install \
+  && sudo bash /tmp/ubuntu-perfect-install/run.sh;
+  sudo rm -rf /tmp/ubuntu-perfect-install
 "
