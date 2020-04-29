258d43cb367afbde57db961c88444dc009b00722
Thiago Tognoli <thiagotognoli@gmail.com>
2020-04-28 22:45:03 -0300

    teste if link exist and copy

--- install.sh
+++ install.sh
@@ -793,7 +793,6 @@ function menu_develtools() {
     options_title+=("BaseX [apt]")
     options_selected+=(TRUE)
     options_id+=("addApt \"basex\"")
-    
 
     options_title+=("MySQL Workbench (Mysql GUI Client) [apt]")
     options_selected+=(TRUE)
@@ -932,236 +931,235 @@ function restore_home_configs() {
 
 
 
-    if [[ -e  "$oldHome/.var" ]]; then
+    if [[ -e "$oldHome/.var" || -L "$oldHome/.var" ]]; then
         options_title+=("Flatpak's Apps Configs")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.var' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/snap" ]]; then
+    if [[ -e "$oldHome/snap" || -L "$oldHome/snap" ]]; then
         options_title+=("Snap's Apps Configs")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/snap' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/basex" ]]; then
+    if [[ -e "$oldHome/basex" || -L "$oldHome/basex" ]]; then
         options_title+=("BaseX Db's")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/basex' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/Applications" ]]; then
+    if [[ -e "$oldHome/Applications" || -L "$oldHome/Applications" ]]; then
         options_title+=("Applications")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/Applications' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/applications" ]]; then
+    if [[ -e "$oldHome/applications" || -L "$oldHome/applications" ]]; then
         options_title+=("applications")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/applications' '$homeDir/'")
     fi
     
-    if [[ -e  "$oldHome/Apps" ]]; then
+    if [[ -e "$oldHome/Apps" || -L "$oldHome/Apps" ]]; then
         options_title+=("Apps")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/Apps' '$homeDir/'")
     fi
     
-    if [[ -e  "$oldHome/apps" ]]; then
+    if [[ -e "$oldHome/apps" || -L "$oldHome/apps" ]]; then
         options_title+=("apps")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/apps' '$homeDir/'")
     fi
     
-    if [[ -e  "$oldHome/bin" ]]; then
+    if [[ -e "$oldHome/bin" || -L "$oldHome/bin" ]]; then
         options_title+=("bin")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/bin' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/appimage" ]]; then
+    if [[ -e "$oldHome/appimage" || -L "$oldHome/appimage" ]]; then
         options_title+=("appimage")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/appimage' '$homeDir/'")
     fi
     
-    if [[ -e  "$oldHome/AppImage" ]]; then
+    if [[ -e "$oldHome/AppImage" || -L "$oldHome/AppImage" ]]; then
         options_title+=("AppImage")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/AppImage' '$homeDir/'")
     fi
-    
 
-    if [[ -e  "$oldHome/VirtualBox VMs" ]]; then
+    if [[ -e "$oldHome/VirtualBox VMs" || -L "$oldHome/VirtualBox VMs" ]]; then
         options_title+=("VirtualBox VM's")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/VirtualBox VMs' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/go" ]]; then
+    if [[ -e "$oldHome/go" || -L "$oldHome/go" ]]; then
         options_title+=("Go")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/go' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.config/Microsoft" || -e  "$oldHome/.config/teams" ]]; then
+    if [[ -e "$oldHome/.config/Microsoft" || -e "$oldHome/.config/teams" || -L "$oldHome/.config/Microsoft" || -L "$oldHome/.config/teams" ]]; then
         options_title+=("Microsoft's Apps Configs (Teams, ...)")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.config/Microsoft' '$homeDir/.config/'; sudo $rsyncCommand '$oldHome/.config/teams' '$homeDir/.config/'")
     fi
 
-    if [[ -e  "$oldHome/.bashrc" ]]; then
+    if [[ -e "$oldHome/.bashrc" || -L "$oldHome/.bashrc" ]]; then
         options_title+=("Bash config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.bashrc' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.profile" ]]; then
+    if [[ -e "$oldHome/.profile" || -L "$oldHome/.profile" ]]; then
         options_title+=("Profile Config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.profile' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.zshrc" ]]; then
+    if [[ -e "$oldHome/.zshrc" || -L "$oldHome/.zshrc" ]]; then
         options_title+=("Zsh Config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.zshrc' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.p10k.zsh" ]]; then
+    if [[ -e "$oldHome/.p10k.zsh" || -L "$oldHome/.p10k.zsh" ]]; then
         options_title+=("Power Level 10K Zsh Config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.p10k.zsh' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.ssh" ]]; then
+    if [[ -e "$oldHome/.ssh" || -L "$oldHome/.ssh" ]]; then
         options_title+=("Ssh Config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.ssh' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.gitconfig" ]]; then
+    if [[ -e "$oldHome/.gitconfig" || -L "$oldHome/.gitconfig" ]]; then
         options_title+=("Git Config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.gitconfig' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.gnupg" ]]; then
+    if [[ -e "$oldHome/.gnupg" || -L "$oldHome/.gnupg" ]]; then
         options_title+=("GnuPG")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.gnupg' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.local/share/keyrings" ]]; then
+    if [[ -e "$oldHome/.local/share/keyrings" || -L "$oldHome/.local/share/keyrings" ]]; then
         options_title+=("Gnome Keyrings")
         options_selected+=(TRUE)
         options_id+=("sudo -u $currentUser mv '$homeDir/.local/share/keyrings' '$homeDir/.local/share/keyrings.old' && sudo -u $currentUser mkdir -p '$homeDir/.local/share/keyrings' && sudo -u $currentUser cp -r '$oldHome/.local/share/keyrings/'{login.keyring,user.keystore} '$homeDir/.local/share/keyrings'")
     fi
     
-    if [[ -e  "$oldHome/.mackup.cfg" ]]; then
+    if [[ -e "$oldHome/.mackup.cfg" || -L "$oldHome/.mackup.cfg" ]]; then
         options_title+=("Mackup Config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.mackup.cfg' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.thunderbird" ]]; then
+    if [[ -e "$oldHome/.thunderbird" || -L "$oldHome/.thunderbird" ]]; then
         options_title+=("Thunderbird Config e E-mails")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.thunderbird' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.config/SiriKali" ]]; then
+    if [[ -e "$oldHome/.config/SiriKali" || -L "$oldHome/.config/SiriKali" ]]; then
         options_title+=("SiriKali Config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.config/SiriKali' '$homeDir/.config/';")
     fi
 
-    if [[ -e  "$oldHome/.SiriKali" ]]; then
+    if [[ -e "$oldHome/.SiriKali" || -L "$oldHome/.SiriKali" ]]; then
         options_title+=("SiriKali Diretórios")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.SiriKali/' '$homeDir/.SiriKali/' --exclude '*/*' --include '*'")
     fi
 
-    if [[ -e  "$oldHome/.wine" ]]; then
+    if [[ -e "$oldHome/.wine" || -L "$oldHome/.wine" ]]; then
         options_title+=("Wine")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.wine' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/Nextcloud" ]]; then
+    if [[ -e "$oldHome/Nextcloud" || -L "$oldHome/Nextcloud" ]]; then
         options_title+=("Nextcloud Repositórios")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/Nextcloud'* '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.config/Nextcloud" ]]; then
+    if [[ -e "$oldHome/.config/Nextcloud" || -L "$oldHome/.config/Nextcloud" ]]; then
         options_title+=("Nextcloud Config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.config/Nextcloud' '$homeDir/.config/'")
     fi
 
-    if [[ -e  "$oldHome/.snap/netbeans" ]]; then
+    if [[ -e "$oldHome/.snap/netbeans" || -L "$oldHome/.snap/netbeans" ]]; then
         options_title+=("Netbeans Snap Config")
         options_selected+=(TRUE)
         options_id+=("sudo -u $currentUser mv '$homeDir/snap/netbeans' '$homeDir/snap/netbeans.old'; sudo $rsyncCommand '$oldHome/snap/netbeans' '$homeDir/snap/'")
     fi
 
-    if [[ -e  "$oldHome/.mysql" ]]; then
+    if [[ -e "$oldHome/.mysql" || -L "$oldHome/.mysql" ]]; then
         options_title+=("MySQL Client Config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.mysql'* '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.config/filezilla" ]]; then
+    if [[ -e "$oldHome/.config/filezilla" || -L "$oldHome/.config/filezilla" ]]; then
         options_title+=("Filezzila Config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.config/filezilla' '$homeDir/.config/'")
     fi
 
-    if [[ -e  "$oldHome/.local/share/gnome-shell" ]]; then
+    if [[ -e "$oldHome/.local/share/gnome-shell" || -L "$oldHome/.local/share/gnome-shell" ]]; then
         options_title+=("Gnome Shell Extensions e Configs")
         options_selected+=(FALSE)
         options_id+=("sudo -u $currentUser mkdir -p '$homeDir/.local/share/gnome-shell/'; sudo $rsyncCommand '$oldHome/.local/share/gnome-shell/extensions' '$homeDir/.local/share/gnome-shell/'")
     fi
 
-    if [[ -e  "$oldHome/.config/google-chrome" ]]; then
+    if [[ -e "$oldHome/.config/google-chrome" || -L "$oldHome/.config/google-chrome" ]]; then
         options_title+=("Google Chrome Dados e Config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.config/google-chrome' '$homeDir/.config/'")
     fi
 
-    if [[ -e  "$oldHome/.mozilla" ]]; then
+    if [[ -e "$oldHome/.mozilla" || -L "$oldHome/.mozilla" ]]; then
         options_title+=("Mozilla Firefox Dados e Config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.mozilla' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.config/transmission" ]]; then
+    if [[ -e "$oldHome/.config/transmission" || -L "$oldHome/.config/transmission" ]]; then
         options_title+=("Transmission config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.config/transmission' '$homeDir/.config/'")
     fi
 
-    if [[ -e  "$oldHome/.config/teamviewer" ]]; then
+    if [[ -e "$oldHome/.config/teamviewer" || -L "$oldHome/.config/teamviewer" ]]; then
         options_title+=("Team Viewer config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.config/teamviewer' '$homeDir/.config/'")
     fi
 
-    if [[ -e  "$oldHome/.config/libreoffice" ]]; then
+    if [[ -e "$oldHome/.config/libreoffice" || -L "$oldHome/.config/libreoffice" ]]; then
         options_title+=("Libre Office config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.config/libreoffice' '$homeDir/.config/'")
     fi
 
-    if [[ -e  "$oldHome/.psensor" ]]; then
+    if [[ -e "$oldHome/.psensor" || -L "$oldHome/.psensor" ]]; then
         options_title+=("PSensor config")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.psensor' '$homeDir/'")
     fi
 
-    if [[ -e  "$oldHome/.fonts" ]]; then
+    if [[ -e "$oldHome/.fonts" || -L "$oldHome/.fonts" ]]; then
         options_title+=("Fonts (User)")    
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldHome/.fonts' '$homeDir/'")
@@ -1290,13 +1288,13 @@ function restore_system_old() {
     options_selected=();
 
 
-    if [[ -e  "$oldRoot/etc/NetworkManager/system-connections" ]]; then
+    if [[ -e "$oldRoot/etc/NetworkManager/system-connections" ]]; then
         options_title+=("Conexões Network Manager")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldRoot/etc/NetworkManager/system-connections' '$currentRoot/etc/NetworkManager/' && sudo service network-manager reload")
     fi
 
-    if [[ -e  "$oldRoot/data" ]]; then
+    if [[ -e "$oldRoot/data" ]]; then
         options_title+=("/data")
         options_selected+=(TRUE)
         options_id+=("sudo $rsyncCommand '$oldRoot/data' '$currentRoot/'")
