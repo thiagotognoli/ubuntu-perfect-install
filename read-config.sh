2dcfc731023be9acca3d186d878e76b64999369b
Thiago Tognoli <thiagotognoli@gmail.com>
2020-05-03 19:45:05 -0300

    readconfig in dev

--- read-config.sh
+++ read-config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 configDir=".config"
+
 tmpDir="tmp"
 
 log_file="$tmpDir/log.log"
@@ -11,183 +12,309 @@ log() {
     echo "$1"
 }
 
-parseGnomeShellExtensions() {
-    gnomeShellExtensionCondigDir="$1"
-    
-    configs="$(find $gnomeShellExtensionCondigDir -name "*.cnf" -exec bash -c "cat {} && echo" \;)"
-    configs="$(echo "$configs" | sed -r -e "s/(#.*)//" -e "/^[[:space:]]*$/d" -e "s/^([[:space:]]*)([^[[:space:]].*)/\2/")"
+parseConfigs() {
 
-    # [code]
-    # id
-    # name
-    # description
-    # url
-    # group
-    # selected
+    local _fileExtension _configDir _configCustomDir
+    _fileExtension="${1}"; _configDir="${2}"; _configCustomDir="${3}";
 
-    code=""
-    id=""
-    name=""
-    description=""
-    url=""
-    group="Geral"
-    selected=false
+    local sectionField sectionRegex sectionRegexValue
+    sectionField="${4}"; sectionRegex="${5}"; sectionRegexValue="${6}"
 
-    regexCode="^\[.*\]"
-    regexId="^id\="
-    regexName="^name\="
-    regexDescription="^description\="
-    regexUrl="^url\="
-    regexGroup="^group\="
-    regexSelected="^selected\="
+    local _fields _fieldsDefaultValue _fieldsRequired _fieldsFilterValue
+    _fields="${7}"
+    _fieldsDefaultValue="${8}"
+    _fieldsRequired="${9}"
+    _fieldsFilterValue="${10}"
 
+    local _sortConfigs
+    _sortConfigs="${11}"
+
+    local configs configsCustom
+    configs="$(find $_configDir -name "*.$_fileExtension" -exec bash -c "cat {} && echo" \;)"
+    configsCustom="$(find $_configCustomDir -name "*.$_fileExtension" -exec bash -c "cat {} && echo" \;)"
+    configs="$(echo -e "$configs\n$configsCustom" | sed -r -e "s/(#.*)//" -e "/^[[:space:]]*$/d" -e "s/^([[:space:]]*)([^[[:space:]].*)/\2/")"
+    configs="$(echo "$configs" | tac)"
+
+    local -A sectionsProcessed
+    local sections=()
+
+    local fields=("$sectionField")
+    eval "fields+=($_fields)"
+
+    local fieldsRequired=("$sectionField")
+    eval "fieldsRequired+=($_fieldsRequired)"
+
+    local -A fieldsFilterValue
+    eval "fieldsFilterValue+=($_fieldsFilterValue)"
+
+    local -A fieldsDefaultValue
+    eval "fieldsDefaultValue+=($_fieldsDefaultValue)"
+
+
+    local sort=false
+    local fieldsToSort=()
+    local -A fieldsToSortTypes
+    local -A fieldsToSortLenght
+
+    local sortConfigs="$_sortConfigs"
+
+    sortConfigs=(${sortConfigs//;/ })
+    local sortConfigRegex="(.*)\:(.*)\,([0-9]*)"
+    for sortConfig in "${sortConfigs[@]}";
+    do
+        if [[ $sortConfig =~ $sortConfigRegex ]]; then
+            sort=true
+            fieldsToSort+=("${BASH_REMATCH[1]}")
+            fieldsToSortTypes["${BASH_REMATCH[1]}"]="${BASH_REMATCH[2]}"
+            fieldsToSortLenght["${BASH_REMATCH[1]}"]="${BASH_REMATCH[3]}"
+        #else
+        #   log "no match"
+        fi
+    done
+
+    local -A fieldsRegex
+    fieldsRegex["$sectionField"]="$sectionRegex"
+
+    local -A fieldsValueRegex
+    fieldsValueRegex["$sectionField"]="$sectionRegexValue"
+
+    for field in "${fields[@]}"
+    do
+        #echo "preenchendo $field"
+        if [ ! "${fieldsDefaultValue["$field"]+true}" ];
+        then
+            fieldsDefaultValue["$field"]=""
+        fi
+        if [ ! "${fieldsRegex["$field"]+true}" ];
+        then
+            #echo "preenchendo regex $field"
+            fieldsRegex["$field"]="^$field\="
+        fi
+        if [ ! "${fieldsValueRegex["$field"]+true}" ];
+        then
+            fieldsValueRegex["$field"]="s/${fieldsRegex[$field]}(.*)/\1/"
+        fi
+    done
+
+    local -A currentFieldsValues
+    for fieldDefault in "${fields[@]}"
+    do
+        currentFieldsValues["$fieldDefault"]="${fieldsDefaultValue["$fieldDefault"]}"
+    done
+
+    declare -A fieldsValues
+    local fieldsValuesOrders=()
+
+    local line
     while read line; do
-        if [[ "$line" =~ $regexCode ]]; then
-            if [[ "$code" && "$id" && "$name" ]]; then
-               echo "code: $code"
-               echo "id: $id"
-               echo "name: $name"
-               echo "descritpion: $descritpion"
-               echo "url: $url"
-               echo "group: $group"
-               echo "selected: $selected"
-            fi
-        code="$(echo "$line" | sed -r "s/^\[([^\[]*)\]/\1/" )"
-        id=""
-        name=""
-        description=""
-        url=""
-        group="Geral"
-        selected=false
+        local field=false
+        for fieldRegex in "${fields[@]}"
+        do
+            local regex="${fieldsRegex[$fieldRegex]}"
+            #echo "Regex: $regex"
+            if [[ "$line" =~ $regex ]]; then
+                field="$fieldRegex"
+                break;
+            fi;
+        done
 
-        elif [[ "$line" =~ $regexId ]]; then
-            id="$(echo "$line" | sed -r "s/$regexId(.*)/\1/")"
-        elif [[ "$line" =~ $regexName ]]; then
-            name="$(echo "$line" | sed -r "s/$regexName(.*)/\1/")"
-        elif [[ "$line" =~ $regexDescription ]]; then
-            description="$(echo "$line" | sed -r "s/$regexDescription(.*)/\1/")"
-        elif [[ "$line" =~ $regexUrl ]]; then
-            url="$(echo "$line" | sed -r "s/$regexUrl(.*)/\1/")"
-        elif [[ "$line" =~ $regexGroup ]]; then
-            group="$(echo "$line" | sed -r "s/$regexGroup(.*)/\1/")"
-        elif [[ "$line" =~ $regexSelected ]]; then
-            selected="$(echo "$line" | sed -r "s/$regexSelected(.*)/\1/")"
-        else
-            echo "Unknown config: $line"
+        if [ "$field" != "false" ]; then
+            local value="$(sed -r "${fieldsValueRegex["$field"]}" <<< "$line" )"
+            currentFieldsValues["$field"]="$(sed -r "${fieldsValueRegex["$field"]}" <<< "$line" )"
+            #echo "preenchendo $field = ${currentFieldsValues["$field"]}"        
+
+            if [ "$field" = "$sectionField" ]; then
+                if [[ "${sectionsProcessed["$value"]}" != "true" ]];
+                then            
+                    #testar requiridos
+                    local requiredTest=true
+                    local fieldRequired
+
+                    for fieldRequired in "${fieldsRequired[@]}";
+                    do
+                        if [ ! "${currentFieldsValues["$fieldRequired"]}" ]; then
+                            requiredTest=false
+                        fi;
+                    done
+                    #testar valores requiridos
+                    for fieldRequired in "${!fieldsFilterValue[@]}";
+                    do
+                        if [[ "${currentFieldsValues["$fieldRequired"]}" != "${fieldsFilterValue["$fieldRequired"]}" ]]; then
+                            requiredTest=false
+                        fi;
+                    done
+
+                    #echo "---requiredTest: $requiredTest"
+                    #imprmir
+                    if [ "$requiredTest" = "true" ]; then
+                        sections+=("$value")
+                        for fieldValue in "${fields[@]}";
+                        do
+                            #echo "$fieldValue: ${currentFieldsValues["$fieldValue"]}"
+                            fieldsValues["$value","$fieldValue"]="${currentFieldsValues["$fieldValue"]}"
+                            fieldsValuesOrders+=("$value","$fieldValue")
+                        done
+                    fi
+                fi
+                sectionsProcessed["$value"]=true
+
+                #limpar fields
+                for fieldDefault in "${fields[@]}"
+                do
+                    currentFieldsValues["$fieldDefault"]="${fieldsDefaultValue["$fieldDefault"]}"
+                done                
+                
+            fi            
         fi
     done <<< "$configs"
 
-    if [[ "$code" && "$id" && "$name" ]]; then
-        echo "code: $code"
-        echo "id: $id"
-        echo "name: $name"
-        echo "descritpion: $descritpion"
-        echo "url: $url"
-        echo "group: $group"
-        echo "selected: $selected"
+
+    if [[ "${sections[@]}" && "$sort" = "true" ]]; then
+        local sectionsToOrderKey=()
+        local -A sectionsOrderKey
+        local orderKey=""
+
+        for section in "${sections[@]}";
+        do
+            for fieldToSort in "${fieldsToSort[@]}";
+            do
+                local value="${fieldsValues["$section","$fieldToSort"]}"
+                case "${fieldsToSortTypes["$fieldToSort"]}" in
+                        "numeric")
+                            value="$(printf "%0${fieldsToSortLenght["$fieldToSort"]}d" "$value")"
+                                ;;
+                        "string")
+                            local fieldValueLenght=${#value}
+                            local padLenght=${fieldsToSortLenght["$fieldToSort"]}
+                            let padLenght=padLenght-fieldValueLenght
+                            value="$(printf "%s%${padLenght}s" "$value")"
+                                ;;
+                esac
+                orderKey+="$value|"
+            done
+            sectionsOrderKey["$orderKey"]="$section"
+            sectionsToOrderKey+=("$orderKey")
+            orderKey=""
+        done
+
+        sections=()
+        local element
+        while IFS= read -r -d '' element; do
+            sections+=( "${sectionsOrderKey["$element"]}" )
+        done < <(printf "%s\0" "${sectionsToOrderKey[@]}" | LC_ALL=C sort -z)
     fi
 
+    for section in "${sections[@]}";
+    do
+        for field in "${fields[@]}";
+        do
+            sectionField="$section,$field"
+            echo "$sectionField: ${fieldsValues["$sectionField"]}"
+        done
+    done
 
+    #caso grupo nao exista crie ?
 }
 
 
-_parseGnomeShellExtensions() {
-    gnomeShellExtensionCondigDir="$configDir/gnome-shell-extension"
-    # set a default cnfiguration file
-    cnf_file="$gnomeShellExtensionCondigDir/teste.cnf"
-
-    # however if there is a host dependant cnf file override it
-    test -f "$gnomeShellExtensionCondigDir/teste.cnf" \
-        && cnf_file="$gnomeShellExtensionCondigDir/teste.cnf"
-
-    ## yet finally override if passed as argument to this function
-    ## if the the ini file is not passed define the default host independant ini file
-    #test -z "$1" || cnf_file=$1; shift 1;
+#group name order select disabled -.gsegroup
 
 
-    test -z "$2" || ini_section=$2; shift 1;
-    log "DEBUG read configuration file : $cnf_file"
-    log "INFO read [$ini_section] section from config file"
+function parseGnomeShellExtensionGroups() {
+    local _fileExtension _configDir _configCustomDir
+    _fileExtension="gsegroup"
+    _configDir="$configDir/default/gnome-shell-extension"
+    _configCustomDir="$configDir/custom/gnome-shell-extension"
 
-    # debug echo "@doParseConfFile cnf_file:: $cnf_file"
-    # coud be later on parametrized ...
-    test -z "$ini_section" && ini_section='MAIN_SETTINGS'
+    local sectionField sectionRegex sectionRegexValue
+    sectionField="name"
+    sectionRegex="^\[.*\]"
+    sectionRegexValue="s/^\[([^\[]*)\]/\1/"
 
-    log "DEBUG reading: the following configuration file"
-    log "DEBUG ""$cnf_file"
-    ( set -o posix ; set ) | sort >"$tmpDir/vars.before"
+    local _fields='"selected" "order" "disabled"'
+    local _fieldsDefaultValue='["selected"]=false ["order"]=99999 ["disabled"]=false'
 
-    configs="$(sed -r "s/(#.*)//" "$currentHomeDir/.config/user-dirs.dirs" | sed '/^[[:space:]]*$/d')"
+    local _fieldsRequired=''
 
+    #local _fieldsFilterValue='["disabled"]=false ["group"]="General"'
+    local _fieldsFilterValue='["disabled"]=false'
+    #local _fieldsFilterValue=''
 
-    eval `sed -e 's/[[:space:]]*\=[[:space:]]*/=/g' \
-        -e 's/#.*$//' \
-        -e 's/[[:space:]]*$//' \
-        -e 's/^[[:space:]]*//' \
-        -e "s/^\(.*\)=\([^\"']*\)$/\1=\"\2\"/" \
-        < $cnf_file \
-        | sed -n -e "/^\[$ini_section\]/,/^\s*\[/{/^[^#].*\=.*/p;}"`
+        
+    local _sortConfigs="order:numeric,10;name:string,250"
+    #local _sortConfigs=""
 
-    ( set -o posix ; set ) | sort >"$tmpDir/vars.after"
-
-    log "INFO added the following vars from section: [$ini_section]"
-    cmd="$(comm -3 $tmpDir/vars.before $tmpDir/vars.after | perl -ne 's#\s+##g;print "\n $_ "' )"
-    echo -e "$cmd"
-    echo -e "$cmd" >> $log_file
-    echo -e "\n\n"
-    #sleep 1; printf "\033[2J";printf "\033[0;0H" # and clear the screen
-
+    parseConfigs \
+        "$_fileExtension" \
+        "$_configDir" \
+        "$_configCustomDir" \
+        "$sectionField" \
+        "$sectionRegex" \
+        "$sectionRegexValue" \
+        "$_fields" \
+        "$_fieldsDefaultValue" \
+        "$_fieldsRequired" \
+        "$_fieldsFilterValue" \
+        "$_sortConfigs"
 }
 
-parseGnomeShellExtensions "$configDir/gnome-shell-extension"
+
+function parseGnomeShellExtensions() {
+    local _fileExtension _configDir _configCustomDir
+    _fileExtension="gsextension"
+    _configDir="$configDir/default/gnome-shell-extension"
+    _configCustomDir="$configDir/custom/gnome-shell-extension"
+
+    local sectionField sectionRegex sectionRegexValue
+    sectionField="code"
+    sectionRegex="^\[.*\]"
+    sectionRegexValue="s/^\[([^\[]*)\]/\1/"
+
+    local _fields='"id" "name" "description" "url" "group" "selected" "order" "disabled"'
+    local _fieldsDefaultValue='["group"]="General" ["selected"]=false ["order"]=99999 ["disabled"]=false'
+
+    local _fieldsRequired='"id" "name"'
+
+    #local _fieldsFilterValue='["disabled"]=false ["group"]="General"'
+    local _fieldsFilterValue='["disabled"]=false'
+    #local _fieldsFilterValue=''
+
+        
+    local _sortConfigs="order:numeric,10;name:string,250"
+    #local _sortConfigs=""
+
+    parseConfigs \
+        "$_fileExtension" \
+        "$_configDir" \
+        "$_configCustomDir" \
+        "$sectionField" \
+        "$sectionRegex" \
+        "$sectionRegexValue" \
+        "$_fields" \
+        "$_fieldsDefaultValue" \
+        "$_fieldsRequired" \
+        "$_fieldsFilterValue" \
+        "$_sortConfigs"
+}
+
+echo "=========================="
+echo "Groups"
+parseGnomeShellExtensionGroups
+
+echo "=========================="
+echo "Extensions"
+parseGnomeShellExtensions
 
 exit
 
-#------------------------------------------------------------------------------
-# parse the ini like $0.$host_name.cnf and set the variables
-# cleans the unneeded during after run-time stuff. Note the MainSection
-# courtesy of : http://mark.aufflick.com/blog/2007/11/08/parsing-ini-files-with-sed
-#------------------------------------------------------------------------------
-doParseConfFile(){
-    # set a default cnfiguration file
-    cnf_file="$run_unit_bash_dir/$run_unit.cnf"
 
-    # however if there is a host dependant cnf file override it
-    test -f "$run_unit_bash_dir/$run_unit.$host_name.cnf" \
-    && cnf_file="$run_unit_bash_dir/$run_unit.$host_name.cnf"
-
-    # yet finally override if passed as argument to this function
-    # if the the ini file is not passed define the default host independant ini file
-    test -z "$1" || cnf_file=$1;shift 1;
-
-
-    test -z "$2" || ini_section=$2;shift 1;
-    doLog "DEBUG read configuration file : $cnf_file"
-    doLog "INFO read [$ini_section] section from config file"
-
-    # debug echo "@doParseConfFile cnf_file:: $cnf_file"
-    # coud be later on parametrized ...
-    test -z "$ini_section" && ini_section='MAIN_SETTINGS'
-
-    doLog "DEBUG reading: the following configuration file"
-    doLog "DEBUG ""$cnf_file"
-    ( set -o posix ; set ) | sort >"$tmp_dir/vars.before"
-
-    eval `sed -e 's/[[:space:]]*\=[[:space:]]*/=/g' \
-        -e 's/#.*$//' \
-        -e 's/[[:space:]]*$//' \
-        -e 's/^[[:space:]]*//' \
-        -e "s/^\(.*\)=\([^\"']*\)$/\1=\"\2\"/" \
-        < $cnf_file \
-        | sed -n -e "/^\[$ini_section\]/,/^\s*\[/{/^[^#].*\=.*/p;}"`
-
-    ( set -o posix ; set ) | sort >"$tmp_dir/vars.after"
-
-    doLog "INFO added the following vars from section: [$ini_section]"
-    cmd="$(comm -3 $tmp_dir/vars.before $tmp_dir/vars.after | perl -ne 's#\s+##g;print "\n $_ "' )"
-    echo -e "$cmd"
-    echo -e "$cmd" >> $log_file
-    echo -e "\n\n"
-    sleep 1; printf "\033[2J";printf "\033[0;0H" # and clear the screen
-}
-#eof func doParseConfFile
+gnome_shell_extension.gsextension
+#config file fields
+# [code]
+# id=<integer>
+# name=<string>
+# description=<string>
+# url=<string>
+# group=<string>
+# selected=true|false
+# disabled=true|false
